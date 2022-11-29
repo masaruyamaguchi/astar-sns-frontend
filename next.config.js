0cf89794593cf32fa721f625a28fc6a91b66a5e2
masaruyamaguchi <62404620+masaruyamaguchi@users.noreply.github.com>
2022-11-30 07:34:07 +0900

    error in profile

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "images.unsplash.com", 
+      "unsplash.com", 
+      "plus.unsplash.com",
+      "ibb.co",
+      "i.ibb.co",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
